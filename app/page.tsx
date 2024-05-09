1c924c5b3f99b57d8a1a642409f5e919193fcb9a
WilliamJizh <65744833+WilliamJizh@users.noreply.github.com>
2024-05-08 21:23:26 -0700

    home page button responsiveness fix

--- app/page.tsx
+++ app/page.tsx
@@ -9,17 +9,15 @@ export default function Home() {
         <div className="sticky top-0 z-40 flex w-full items-center justify-center border-b backdrop-blur supports-[backdrop-filter]:bg-background/80 lg:h-screen lg:justify-end lg:border-b-0 lg:border-r">
           <div className="flex h-full max-w-md p-8">
             <div className="m-auto grid items-center justify-center">
-              <div className="absolute z-10 h-64 w-64 rounded-full bg-gradient-to-b from-white/30 to-transparent blur-3xl pointer-events-none"></div>
+              <div className="pointer-events-none absolute z-10 h-64 w-64 rounded-full bg-gradient-to-b from-white/30 to-transparent blur-3xl"></div>
               <h1 className="text-4xl font-bold">Making A Tier List?</h1>
               <p className="mt-2 text-zinc-400">
                 A tier list is a concept originating in video game culture where
                 playable characters or other in-game elements are subjectively
                 ranked by their respective viability as part of a list.
               </p>
-              <Button className="m-auto mt-6">
-                <Link href={"/create"}>
-                  Create Your List
-                </Link>
+              <Button className="m-auto mt-6" asChild>
+                <Link href={"/create"}>Create Your List</Link>
               </Button>
             </div>
           </div>
